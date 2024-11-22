a5475cf34ed5288d6fe3f3d4d8eb30301a6ab7bd
Nur Mufidah <mufidah@alumni.pcr.ac.id>
2024-11-22 11:32:41 +0700

    ubah url

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/mahrus', function () {
+Route::get('/masdjkbasjkd', function () {
     return view('welcome');
 });
